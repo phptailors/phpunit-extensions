e93c6ddede3cd33de514d5ec5038586245e6271c
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2020-10-06 22:03:14 +0200

    fix copyright notice

--- packages/phpunit-properties/src/Properties/ExpectedPropertiesDecoratorTrait.php
+++ packages/phpunit-properties/src/Properties/ExpectedPropertiesDecoratorTrait.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of php-tailors/phpunit-extensions.
  *
- * (c) Paweł Tomulik <ptomulik@meil.pw.edu.pl>
+ * Copyright (c) Paweł Tomulik <ptomulik@meil.pw.edu.pl>
  *
  * View the LICENSE file for full copyright and license information.
  */
