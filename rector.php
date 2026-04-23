2f5f4fbadce2caa7f218e4c87b93d75c8801ed8d
Paweł Tomulik <pawel@tomulik.pl>
2026-04-23 12:37:06 +0200

    rector: withPreparedSets(phpunit: true)

--- rector.php
+++ rector.php
@@ -1,12 +1,28 @@
 <?php
 
 use Rector\Config\RectorConfig;
-use Rector\PHPUnit\AnnotationsToAttributes\Rector\ClassMethod\DataProviderAnnotationToAttributeRector;
-use Rector\PHPUnit\AnnotationsToAttributes\Rector\Class_\CoversAnnotationWithValueToAttributeRector;
+use Rector\PHPUnit\PHPUnit60\Rector\ClassMethod\AddDoesNotPerformAssertionToNonAssertingTestRector;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/packages/*/tests/src/',
+        __DIR__.'/packages/*/src/',
+    ])
+    ->withPreparedSets(
+        phpunit: true
+    )
+    ->withSkip([
+        AddDoesNotPerformAssertionToNonAssertingTestRector::class => [
+            __DIR__.'/packages/phpunit-arrays/tests/src/Constraint/ArrayValuesEqualToTest.php',
+            __DIR__.'/packages/phpunit-arrays/tests/src/Constraint/ArrayValuesIdenticalToTest.php',
+            __DIR__.'/packages/phpunit-arrays/tests/src/Constraint/KsortedArrayEqualToTest.php',
+            __DIR__.'/packages/phpunit-arrays/tests/src/Constraint/KsortedArrayIdenticalToTest.php',
+            __DIR__.'/packages/phpunit-methods/tests/src/Constraint/HasMethodTest.php',
+            __DIR__.'/packages/phpunit-properties/tests/src/Constraint/ClassPropertiesEqualToTest.php',
+            __DIR__.'/packages/phpunit-properties/tests/src/Constraint/ClassPropertiesIdenticalToTest.php',
+            __DIR__.'/packages/phpunit-properties/tests/src/Constraint/ObjectPropertiesEqualToTest.php',
+            __DIR__.'/packages/phpunit-properties/tests/src/Constraint/ObjectPropertiesIdenticalToTest.php',
+        ]
     ])
     ->withRules([
     ])
