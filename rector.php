8f0718ca987fa25a3ea4ade45b1add41dca50ebc
Paweł Tomulik <pawel@tomulik.pl>
2026-04-28 15:48:18 +0200

    disabled ArrowFunctionDelegatingCallToFirstClassCallableRector

--- rector.php
+++ rector.php
@@ -1,5 +1,6 @@
 <?php
 
+use Rector\CodingStyle\Rector\ArrowFunction\ArrowFunctionDelegatingCallToFirstClassCallableRector;
 use Rector\Config\RectorConfig;
 use Rector\PHPUnit\PHPUnit60\Rector\ClassMethod\AddDoesNotPerformAssertionToNonAssertingTestRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
@@ -32,7 +33,10 @@ return RectorConfig::configure()
             __DIR__.'/packages/phpunit-properties/tests/src/Constraint/ClassPropertiesIdenticalToTest.php',
             __DIR__.'/packages/phpunit-properties/tests/src/Constraint/ObjectPropertiesEqualToTest.php',
             __DIR__.'/packages/phpunit-properties/tests/src/Constraint/ObjectPropertiesIdenticalToTest.php',
-        ]
+        ],
+        // Psalm 6.x has bug: https://github.com/vimeo/psalm/issues/11038
+        // We'll avoid first-class callables for the moment and prefer delegation.
+        ArrowFunctionDelegatingCallToFirstClassCallableRector::class => true,
     ])
     ->withRules([
     ])
